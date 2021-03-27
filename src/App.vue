0ed33dc5f2c0766725e9320c279c44c1661ffd31
RilDev <c.public@tutanota.com>
2021-03-27 13:03:14 +0100

    add rotate & reset

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,54 @@
 <template>
   <div class="bg-gray-800 h-screen flex items-center justify-center">
     <main class="bg-gray-300 max-w-lg p-6 rounded-md flex">
-      <section class="flex flex-col mr-6">
-        <label for="perspective">Perspective</label>
-        <input
-          v-model="perspective"
-          type="range"
-          min="0"
-          max="1000"
-          id="perspective"
-          class="bg-gray-800"
-        />
+      <section class="flex flex-col justify-between mr-6">
+        <div class="flex flex-col">
+          <label for="perspective">Perspective</label>
+          <input
+            v-model="perspective"
+            type="range"
+            min="0"
+            max="1000"
+            id="perspective"
+            class="bg-gray-800"
+          />
 
-        <label for="rotate-x">Rotate X</label>
-        <input type="range" min="-180" max="180" id="rotate-x" />
+          <label for="rotate-x">Rotate X</label>
+          <input
+            v-model="rotateX"
+            type="range"
+            min="-180"
+            max="180"
+            id="rotate-x"
+          />
 
-        <label for="rotate-y">Rotate Y</label>
-        <input type="range" min="-180" max="180" id="rotate-y" />
+          <label for="rotate-y">Rotate Y</label>
+          <input
+            v-model="rotateY"
+            type="range"
+            min="-180"
+            max="180"
+            id="rotate-y"
+          />
 
-        <label for="rotate-z">Rotate Z</label>
-        <input type="range" min="-180" max="180" id="rotate-z" />
+          <label for="rotate-z">Rotate Z</label>
+          <input
+            v-model="rotateZ"
+            type="range"
+            min="-180"
+            max="180"
+            id="rotate-z"
+          />
+        </div>
+
+        <div>
+          <button
+            @click="reset"
+            class="px-3 py-2 bg-blue-500 text-white text-sm rounded-md uppercase tracking-wider"
+          >
+            Reset
+          </button>
+        </div>
       </section>
       <section>
         <div
@@ -29,7 +58,7 @@
           <div
             :style="{
               transformStyle: `preserve-3d`,
-              transform: `rotateX(50deg)`,
+              transform: `rotateX(${rotateX}deg) rotateY(${rotateY}deg) rotateZ(${rotateZ}deg)`,
             }"
             class="bg-purple-700 text-white w-16 h-16 flex items-center justify-center absolute"
           >
@@ -45,10 +74,16 @@
 import { ref, computed, watch } from "vue";
 
 // dynamic variables
-const perspective = ref(0);
+const perspective = ref(500);
+const rotateX = ref(0);
+const rotateY = ref(0);
+const rotateZ = ref(0);
 
 // watch
-watch(perspective, () => console.log(perspective.value));
+watch(perspective, () => console.log(`perspective: ${perspective.value}`));
+watch(rotateX, () => console.log(`rotateX: ${rotateX.value}`));
+watch(rotateY, () => console.log(`rotateY: ${rotateY.value}`));
+watch(rotateZ, () => console.log(`rotateZ: ${rotateZ.value}`));
 
 // computed
 const containerStyles = computed(() => {
@@ -63,6 +98,13 @@ const elementStyles = computed(() => {
     transform: `rotateX(-50deg)`,
   };
 });
+
+const reset = () => {
+  perspective.value = 500;
+  rotateX.value = 0;
+  rotateY.value = 0;
+  rotateZ.value = 0;
+};
 </script>
 
 <style>
