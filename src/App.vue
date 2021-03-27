3794d37de4a61b38cd369c688391e80baf787ef7
RilDev <c.public@tutanota.com>
2021-03-27 18:41:57 +0100

    add dynamic copied button

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-10 py-24 bg-gray-800 h-screen">
+  <div class="px-10 py-24 bg-gray-800 h-screen flex flex-col justify-between">
     <header>
       <div class="text-5xl text-gray-50 text-center font-bold">
         Perspective Playground
@@ -79,15 +79,15 @@
           <div>
             <button
               @click="reset"
-              class="px-3 py-2 mr-2 bg-blue-500 text-white text-sm rounded-md uppercase tracking-wider"
+              class="px-3 py-2 mr-2 bg-blue-500 hover:bg-blue-400 active:bg-blue-600 text-white text-sm rounded-md uppercase tracking-wider transition"
             >
               Reset
             </button>
             <button
               @click="copy"
-              class="px-3 py-2 bg-green-500 text-white text-sm rounded-md uppercase tracking-wider"
+              class="px-3 py-2 bg-green-500 hover:bg-green-400 active:bg-green-600 text-white text-sm rounded-md uppercase tracking-wider transition"
             >
-              Copy
+              {{ computedCopyText }}
             </button>
           </div>
         </section>
@@ -106,6 +106,31 @@
         </section>
       </main>
     </div>
+    <footer class="flex justify-center text-gray-50">
+      <a
+        href="https://rildev.website"
+        target="_blank"
+        class="text-blue-500 hover:text-blue-400 mr-1"
+        >RilDev</a
+      >
+      &copy;
+      <a
+        href="https://github.com/RilDev/perspective-playground"
+        target="_blank"
+        class="flex text-blue-500 hover:text-blue-400 ml-1"
+        ><svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          class="fill-current mr-1"
+        >
+          <path
+            d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z"
+          />
+        </svg>
+        GitHub</a
+      >
+    </footer>
   </div>
 </template>
 
@@ -118,6 +143,8 @@ const defaultRotate = 0;
 const defaultText = "Choose your text!";
 const defaultTextColor = "#ffffff";
 const defaultBackgroundColor = "#6d28d9";
+const defaultCopyText = "Copy";
+const copiedText = "Copied!";
 
 // dynamic variables
 const perspective = ref(defaultPerspective);
@@ -127,6 +154,7 @@ const rotateZ = ref(defaultRotate);
 const textValue = ref(defaultText);
 const textColor = ref(defaultTextColor);
 const backgroundColor = ref(defaultBackgroundColor);
+const isCopied = ref(false);
 
 // computed
 const containerStyles = computed(() => {
@@ -140,6 +168,14 @@ const elementStyles = computed(() => {
     transformStyle: `preserve-3d`,
     transform: `rotateX(${rotateX.value}deg) rotateY(${rotateY.value}deg) rotateZ(${rotateZ.value}deg)`,
   };
+});
+
+const computedCopyText = computed(() => {
+  if (isCopied.value) {
+    return copiedText;
+  }
+
+  return defaultCopyText;
 });
 
 // methods
@@ -179,6 +215,12 @@ const copy = () => {
   element.select();
   document.execCommand("copy");
   document.body.removeChild(element);
+
+  // show that content was copied
+  isCopied.value = true;
+  setTimeout(() => {
+    isCopied.value = false;
+  }, 3000);
 };
 </script>
 
